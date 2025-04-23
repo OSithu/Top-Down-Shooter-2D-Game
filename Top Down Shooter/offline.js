abbd6dd6a30b45fb0d6ab45866ee5875b1a0dc6e
Osh Disanayake <107048011+OSithu@users.noreply.github.com>
2025-04-23 17:07:08 +0530

    Add files via upload

---
+++ Top Down Shooter/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1745407582,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/particles.png",
+		"images/asset14x-sheet0.png",
+		"images/asset14x-sheet1.png",
+		"images/asset14x-sheet2.png",
+		"images/asset14x-sheet3.png",
+		"images/asset14x-sheet4.png",
+		"images/asset14x-sheet5.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
